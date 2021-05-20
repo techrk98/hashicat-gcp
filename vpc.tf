177d9dd829796b2673076a195eaa179e898d4b50
Ramakrishna Rotte <techrk98@gmail.com>
2021-05-20 17:31:24 +0000

    using the network module from private registry

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network" {
+  source  = "app.terraform.io/Techrk98-Training/network/google"
+  version = "2.5.0"
+  # insert required variables here
+  network_name = "db_net"
+  project_id = var.project
+  subnets = [
+  {
+    subnet_name   = "gram-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
